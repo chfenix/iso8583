1fc3f8a3a748c35b40fa9bf612b0e45fb13973f5
zhouluning <zhouluning@816f2b58-3739-4ec1-b617-b17f75f31d01>
2015-07-09 08:21:40 +0000

    修正目录错误

--- src/test/java/cn/agiledata/iso8583/TestCpus.java
+++ src/test/java/cn/agiledata/iso8583/TestCpus.java
@@ -2,6 +2,11 @@ package cn.agiledata.iso8583;
 
 import org.junit.Test;
 
+import cn.agiledata.iso8583.entity.SignRequest;
+import cn.agiledata.iso8583.entity.SignResponse;
+import cn.agiledata.iso8583.util.ISO8583Socket;
+import cn.agiledata.iso8583.util.ISO8583Util;
+
 /**
  * 银商预付卡
  * 
@@ -11,11 +16,52 @@ import org.junit.Test;
 public class TestCpus extends TestBase {
 
 	@Test
-	public void signIn() {
+	public void signIn() throws Exception {
 		try {
-			System.out.println("test");
+			// 设置签到报文
+			SignRequest objSignReq = new SignRequest();
+			objSignReq.setMerNo("123456789111115");		// 商户号
+			objSignReq.setTerminalNo("11115007");	// 终端号
+			
+			// 使用秒数生成批次号及流水号
+			String[] arrTransNo = getBatchAndSeqNo(null);
+			
+			objSignReq.setBatchNo(arrTransNo[0]);	// 批次号
+			objSignReq.setTraceNo(arrTransNo[1]);	// 交易号
+			
+			// 设置签到参数
+			objSignReq.setTransType("00");		// 交易类型:签到
+			objSignReq.setDesType("003");		// 3DES加密
+			objSignReq.setOperator("000");		// 操作员
+			
+			// 发送签到请求
+			SignResponse objSignResp;
+			Message8583 message = MessageFactory.createMessage(MessageFactory.MSG_SPEC_CUPS, objSignReq.getCode(),null);
+			message.fillBodyData(objSignReq);
+			message.pack();
+			byte[] request = message.getMessage();
+			System.out.println(ISO8583Util.printBytes(request));
+			
+			ISO8583Socket socket = new ISO8583Socket();
+			socket.connect("116.228.21.162",4008,5000);
+			
+			socket.sendRequest(request);
+			
+			// 获取返回结果
+			byte[] response = socket.get8583Response(message.getRespLen());
+			Message8583 msgResponse = MessageFactory.createMessage(MessageFactory.MSG_SPEC_CUPS, objSignReq.getCode(),null);
+			msgResponse.setResponse(response);
+			msgResponse.unpack();
+			
+			objSignResp = new SignResponse();
+			msgResponse.getResponseData(objSignResp);
+			objSignResp.process();
+			
+			System.out.println(objSignResp.getRespCode() + " "  + objSignResp.getRespMsg());
+			
 		} catch (Exception e) {
 			e.printStackTrace();
+			throw e;
 		}
 	}
 }
